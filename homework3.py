ead85dffe4675fc2daf718e1e988a1e2205ab38e
Anton801106 <listboxx@yandex.ru>
2024-07-11 18:24:51 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,12 @@
+name = "Anton"
+print(name, type(name))
+age = 43
+print(age, type(age))
+new_age = 45
+print(new_age, type(new_age))
+occupation = "student"
+print(occupation, type(True))
+print('Name:' , name,)
+print('Age:', age)
+print('New age:', new_age)
+print('Occupation:', occupation)
