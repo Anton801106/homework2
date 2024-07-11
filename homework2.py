f08afcf3a30621ae96e362ca62aea90fd3bffb24
Anton801106 <listboxx@yandex.ru>
2024-07-11 18:21:53 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+all_tasks = '12'
+all_time = '1.5'
+time_per_tasck = 'all_time / all_tasks'
+course_name = 'Pyton'
+print('Курс:', course_name, 'всего задач:', all_tasks, 'затрачено часов:', all_time, 'среднее время выполнения:', 0.125)
